9895c05c19ccfc18ed3e16fae3587fb971ffdc67
IreneBioque <irenebioque@gmail.com>
2021-09-01 09:10:18 +0200

    add global const

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 'use estrict';
+const input = document.querySelector('.js_input');
+const button = document.querySelector('.js_button');
+const clue = document.querySelector('.js_paragraph1');
+const trys = document.querySelector('.js_paragraph2');
