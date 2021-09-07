ed40786d434225b0686e1a755dd82ee8b415b594
IreneBioque <irenebioque@gmail.com>
2021-09-07 20:32:19 +0200

    fixed ev.intermedia

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-'use estrict';
+'use estrict'
+
 const input = document.querySelector('.js_input');
 let button = document.querySelector('.js_button');
 const clue = document.querySelector('.js_paragraph1');
 const trys = document.querySelector('.js_paragraph2');
-
-
+const main = document.querySelector('.main_js');
+let counter = 0;
  function getRandomNumber(max) {
  return Math.ceil(Math.random() * max);
  }
@@ -13,31 +14,41 @@ const trys = document.querySelector('.js_paragraph2');
 const randomNumber = getRandomNumber(100);
 console.log(randomNumber);
 
+
 function number() {
     const numeroIntroducido = input.value;
-    //debugger;
-if (numeroIntroducido < 0) {
-    clue.innerHTML= ' El número debe estar entre 1 y 100.'
-} else if (numeroIntroducido > randomNumber && numeroIntroducido < 100) {
-    clue.innerHTML+= ' Demasiado alto.' 
-} else if (numeroIntroducido < randomNumber) {
-    clue.innerHTML+= 'Demasiado bajo.'
-} else if (numeroIntroducido > 100) {
-    clue.innerHTML+= ' El número debe estar entre 1 y 100.'
-
-} else {
-    clue.innerHTML+= ' Has ganado campeona!!!'
-}
+    // debugger;
+    if (isNaN(numeroIntroducido)) {
+         clue.innerHTML = 'Pista: Tienes que escribir un número entero'
+    } else if (numeroIntroducido < 0 || numeroIntroducido > 100) {
+        clue.innerHTML = 'Pista: El número debe estar entre 1 y 100.'
+    } else if (numeroIntroducido > randomNumber) {
+        clue.innerHTML = 'Pista: Demasiado alto.'
+    } else if (numeroIntroducido < randomNumber) {
+       clue.innerHTML = 'Pista: Demasiado bajo.'
+    } else if (numeroIntroducido > 100) {
+       clue.innerHTML = 'Pista: El número debe estar entre 1 y 100.'
+    } else {
+      clue.innerHTML = '¡¡¡ Has ganado campeona!!!'
+    }
+    return numeroIntroducido;
 };
+
 function clickedButton() {
-    const number = 0;
-    trys.innerHTML += number;
+    trys.innerHTML = `Número de intentos: ${counter}`;
 }
-function handleButton() {
-    number();
+function updateCounter() {
+    counter = counter + 1;
     clickedButton();
+}
+function handleButton(ev) {
+    ev.preventDefault();
+    number();
+    updateCounter();
 };
 
 
 
 button.addEventListener('click', handleButton);
+
+
